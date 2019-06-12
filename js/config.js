5323f1b8faaf9c02926983a5a70dd0918b0a12ef
Hervé Nivon <herve.nivon@gmail.com>
2019-06-12 10:40:46 +0200

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_JTi25cDVB',
+        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
+        region: 'eu-west-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
