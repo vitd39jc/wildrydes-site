a908f89dcef737f70f00f9afec960340f45b3ffa
Hervé Nivon <herve.nivon@gmail.com>
2019-06-12 11:24:30 +0200

    configure api invokeURL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
     }
 };
